d25cb83ca9c8ad916096e7d412c5bc09c58ab432
David Jashi <datojashi@gmail.com>
2019-12-01 22:41:14 +0400

    Fix GBLThread

---
+++ gblthread.cpp
@@ -0,0 +1,29 @@
+#include "gblthread.h"
+
+GBLThread::GBLThread()
+{
+
+}
+
+GBLThread::~GBLThread()
+{
+
+}
+
+
+void GBLThread::start()
+{
+    thr=thread(&GBLThread::run,this);
+}
+
+void GBLThread::run()
+{
+
+
+}
+
+void GBLThread::wait()
+{
+
+    thr.join();
+}
